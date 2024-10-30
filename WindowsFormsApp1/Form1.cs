13629d0e6a4878d7095f3cda96e55676c26e509f
FORMAT-2 <mk.408198@gmail.com>
2024-10-30 11:51:23 +0530

    m

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Accusoft.ImagXpressSdk;
+using Accusoft.FormDirectorSdk;
+using Accusoft.FormFixSdk;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Button buttonSelectImage1;
+        private Button buttonSelectImage2;
+        private Button buttonCompare;
+        private PictureBox pictureBoxImage1;
+        private PictureBox pictureBoxImage2;
+        private Label labelImage1Status;
+        private Label labelImage2Status;
+        private string imagePath1;
+        private string imagePath2;
+        public Form1()
+        {
+            InitializeComponent();
+            CreateUI();
+        }
+
+        private void CreateUI()
+        {
+            // Create and configure Button for selecting Image 1
+            buttonSelectImage1 = new Button
+            {
+                Text = "Select Image 1",
+                Location = new Point(30, 30),
+                Size = new Size(120, 30)
+            };
+            buttonSelectImage1.Click += ButtonSelectImage1_Click;
+
+            // Create and configure Button for selecting Image 2
+            buttonSelectImage2 = new Button
+            {
+                Text = "Select Image 2",
+                Location = new Point(30, 80),
+                Size = new Size(120, 30)
+            };
+            buttonSelectImage2.Click += ButtonSelectImage2_Click;
+
+            // Create and configure Button for comparing images
+            buttonCompare = new Button
+            {
+                Text = "Compare Images",
+                Location = new Point(30, 130),
+                Size = new Size(120, 30)
+            };
+            buttonCompare.Click += ButtonCompare_Click;
+
+            // Create and configure PictureBox for Image 1
+            pictureBoxImage1 = new PictureBox
+            {
+                Location = new Point(180, 30),
+                Size = new Size(200, 200),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            // Create and configure PictureBox for Image 2
+            pictureBoxImage2 = new PictureBox
+            {
+                Location = new Point(180, 250),
+                Size = new Size(200, 200),
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                BorderStyle = BorderStyle.FixedSingle
+            };
+
+            // Create and configure Label for Image 1 status
+            labelImage1Status = new Label
+            {
+                Text = "Image 1: Not selected",
+                Location = new Point(30, 260),
+                AutoSize = true
+            };
+
+            // Create and configure Label for Image 2 status
+            labelImage2Status = new Label
+            {
+                Text = "Image 2: Not selected",
+                Location = new Point(30, 310),
+                AutoSize = true
+            };
+
+            // Add controls to the form
+            this.Controls.Add(buttonSelectImage1);
+            this.Controls.Add(buttonSelectImage2);
+            this.Controls.Add(buttonCompare);
+            this.Controls.Add(pictureBoxImage1);
+            this.Controls.Add(pictureBoxImage2);
+            this.Controls.Add(labelImage1Status);
+            this.Controls.Add(labelImage2Status);
+
+            // Configure the form
+            this.ClientSize = new Size(400, 500);
+            this.Text = "Image Fetcher";
+        }
+
+        private void ButtonSelectImage1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select Image 1";
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.tif;*.frs|All files (*.*)|*.*"; // Image types filter
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    imagePath1 = openFileDialog.FileName;
+                    //pictureBoxImage1.Image = Image.FromFile(imagePath1);
+                    labelImage1Status.Text = $"Image 1: {System.IO.Path.GetFileName(imagePath1)}";
+                }
+            }
+        }
+
+        private void ButtonSelectImage2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Select Image 2";
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.tif;*.frs|All files (*.*)|*.*"; // Image types filter
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    imagePath2 = openFileDialog.FileName;
+                    //pictureBoxImage2.Image = Image.FromFile(imagePath2);
+                    labelImage2Status.Text = $"Image 2: {System.IO.Path.GetFileName(imagePath2)}";
+                }
+            }
+        }
+
+        private void ButtonCompare_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(imagePath1) || string.IsNullOrEmpty(imagePath2))
+            {
+                MessageBox.Show("Please select both images before comparing.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Compare images
+            bool areSameSize = CompareImageSizes(imagePath1, imagePath2);
+            string message = areSameSize ? "The images are the same size." : "The images are different sizes.";
+            MessageBox.Show(message, "Comparison Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private bool CompareImageSizes(string path1, string path2)
+        {
+            return FormsProcessing(path1, path2);
+        }
+        public bool FormsProcessing(string formSetFile, string inputFile)
+        {
+            int Result = 0;
+            int page = 1;
+            Accusoft.FormDirectorSdk.FormSetFile myFormSet;
+        //Accusoft.ImagXpressSdk.ImageXData qryFile = null;
+        //if (qryFile.Format != Accusoft.ImagXpressSdk.ImageXFormat.Bmp && qryFile.Format != Accusoft.ImagXpressSdk.ImageXFormat.Tiff
+        //        && qryFile.Format != Accusoft.ImagXpressSdk.ImageXFormat.Jpeg && qryFile.Format != Accusoft.ImagXpressSdk.ImageXFormat.Pdf)
+        //{
+
+        //    return false;
+        //}
+        //try
+        //{
+        //    using (ImageDataIX imgData = new ImageDataIX(formName, 0))
+        //    {
+        //        templateImage = imgData.Load();
+        //    }
+        //}
+        //catch (Exception ex)
+        //{
+        //    return false;
+        //}
+        myFormSet = new Accusoft.FormDirectorSdk.FormSetFile(Accusoft.FormDirectorSdk.FormDirector);
+            myFormSet.Name = "NewFormSet";
+            myFormSet.Filename = "NewFormSet.frs";
+            Accusoft.Forms.LicenseKeychain licenseKeychain = new Accusoft.Forms.LicenseKeychain();
+            using (Accusoft.Forms.Processor formsProcessor = new Accusoft.Forms.Processor(formSetFile, licenseKeychain))
+            using (ImagXpress imagXpress = new ImagXpress())
+            {
+                if (imagXpress.Licensing.LicenseStatus == LicenseStatus.NoLicense)
+                {
+                    imagXpress.Licensing.SetSolutionName("Accusoft");
+                    imagXpress.Licensing.SetSolutionKey(0x00000000, 0x00000000, 0x00000000, 0x00000000);
+                    imagXpress.Licensing.SetOEMLicenseKey("2.0.R5liQmv6TbTj8mpaUDQj86TKEj8bq61X8DE5vJ1bED8Dl4ZI1gvgQDlDvb7bfgVKZbQCpaTCQIEaqRZ4WgU6qXMmp6TjMgvKEg76U5qJEavI8bfXZDljpKTJ7KMJVmZIVDM4VXTJTXVXWKURva8aq4EmMClbVaTJlKEXqXUm7iQDfCEI1j75VjlREC7JTRUjvaWmUgvaW6fCZgqjfDMi8mUmEg1DQ5MblCQJpI8D86ZJ75V6l61KEgEmWI7gfaQC8RqIWbEiU6pRVbqRVgWD1jWm86Vap5W5TaE6f68DTRVbUaMaUXM4pC151CvJvDvRZCQiWIpgUI7i1I85qgE5ZJQDfb8mZCW5VXlbMjWIUITi1RWjfmq5WifmZ57bqb1C7DqRQJQCVR1aUIvgQJqDZD7jVRWRqmqCp41DfX76v6ljMafDlXEXZJUjZaEJ1Ivb7RTXWXWmVIlIlaliZJUgWm7bqRQDWbUK1aZ6WRWmqKMDQg1KlbqgvCva16vIZjZJQIMDvDZgv58gfjVR75vgZgqaZiMbVgE5M6VaTIWCE5lQppK");
+                }
+                Accusoft.ImagXpressSdk.ImageXData qryFile = null;
+                qryFile = ImageX.QueryFile(imagXpress, inputFile);
+                //ImageXData numPages = ImageX.NumPages(imagXpress, inputFile);
+                using (ImageX filledFormImageX = ImageX.FromFile(imagXpress, inputFile, page))
+                using (Bitmap filledFormImage = filledFormImageX.ToBitmap(false))
+                {
+                    Accusoft.Forms.FormResult formResult = null;
+                    formResult = formsProcessor.ProcessImage(filledFormImage);
+                    if (formResult.IdentificationResult.State != Accusoft.Forms.IdentificationState.MatchFound)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+            }
+
+           
+        }
+
+        private int giveResult(Accusoft.Forms.FormResult formResult, ImageX filledFormImage, string fileName, int page)
+        {
+            int Result = 0;
+            if (formResult.IdentificationResult.State != Accusoft.Forms.IdentificationState.MatchFound)
+            {
+                Result = -1;
+            }
+            else
+            {
+                Result = 1;
+            }
+            return Result;
+        }
+
+        public Bitmap Load(string filename)
+        {
+            ImageX imgX = null;
+            Accusoft.ImagXpressSdk.ImagXpress Imagxpress = null;
+            //imgX = ImageX.FromFile(AccuComponent.ImagXpress, this.FileName, this.ImageNum + 1);
+            //else
+                imgX = ImageX.FromFile(Imagxpress, filename);
+
+            Bitmap bmapImage = imgX.ToBitmap(true);
+            imgX.Dispose();
+
+            return bmapImage;
+        }
+
+    }
+}
---
+++ WindowsFormsApp1/ImageDataIX.cs
@@ -0,0 +1,185 @@
+﻿///*************************************************************************
+// * Copyright © 2011-2021 Accusoft Corporation.                           *
+// * This sample code is provided to Accusoft licensees "as is"            *
+// * with no restrictions on use or modification. No warranty for          *
+// * use of this sample code is provided by Accusoft.                      *
+// ************************************************************************/
+
+//namespace WindowsFormsApp1
+//{
+//    using System;
+//    using System.Drawing;
+//    using System.IO;
+//    using Accusoft.ImagXpressSdk;
+
+//    public class ImageDataIX : IImageData, IDisposable
+//    {
+//        private bool _disposed = false;
+//        private Bitmap _cacheBitmap = null;
+
+//        /// <summary>
+//        /// Constructor - filePath only (default/ignored image #)
+//        /// </summary>
+//        /// <param name="filePath"></param>
+//        public ImageDataIX(string fileName) : this(fileName, -1) { }
+
+//        /// <summary>
+//        /// Constructor - filePath and image #
+//        /// </summary>
+//        /// <param name="filePath"></param>
+//        /// <param name="imageNum"></param>
+//        public ImageDataIX(string fileName, int imageNum)
+//        {
+//            _disposed = true;
+
+//            this.FileName = fileName;
+//            this.ImageNum = imageNum;
+
+//            this.CanLoad = true;
+//            this.CanSave = false;   // read-only
+//        }
+
+//        /// <summary>
+//        /// IDisposable method
+//        /// </summary>
+//        public void Dispose()
+//        {
+//            Dispose(true);
+//            GC.SuppressFinalize(this);
+//        }
+
+//        /// <summary>
+//        /// Internal dispose method
+//        /// </summary>
+//        /// <param name="disposing"></param>
+//        protected virtual void Dispose(bool disposing)
+//        {
+//            // If you need thread safety, use a lock around these 
+
+//            if (!_disposed)
+//            {
+//                if (disposing)
+//                {
+//                    if (_cacheBitmap != null)
+//                        _cacheBitmap.Dispose();
+//                }
+
+//                // Indicate that the instance has been disposed.
+//                _cacheBitmap = null;
+//                _disposed = true;
+//            }
+//        }
+
+//        /// <summary>
+//        /// Evaluate whether object can proces the provided image specification string
+//        /// </summary>
+//        /// <param name="fileName"></param>
+//        /// <returns>true = can process</returns>
+//        public static bool CanProcess(string imageSpec)
+//        {
+//            bool supported = Path.GetExtension(imageSpec).ToUpper().Equals(".TIF") ||
+//                             Path.GetExtension(imageSpec).ToUpper().Equals(".TIFF") ||
+//                             Path.GetExtension(imageSpec).ToUpper().Equals(".JPG") ||
+//                             Path.GetExtension(imageSpec).ToUpper().Equals(".JPEG") ||
+//                             Path.GetExtension(imageSpec).ToUpper().Equals(".BMP") ||
+//                             Path.GetExtension(imageSpec).ToUpper().Equals(".PDF");
+
+//            return supported;
+//        }
+
+//        /// <summary>
+//        /// Gets flags indicating whether the object can load or save images
+//        /// </summary>
+//        public bool CanLoad { get; protected set; }
+//        public bool CanSave { get; protected set; }
+
+//        /// <summary>
+//        /// Gets display name string
+//        /// </summary>
+//        public string DisplayName
+//        {
+//            get
+//            {
+//                string dispName = this.FileName;
+
+//                // Combine filePath & image number
+//                if (this.ImageNum >= 0)
+//                    dispName = string.Format("{0}, image {1:D}", this.FileName, this.ImageNum + 1);
+
+//                return dispName;
+//            }
+//        }
+
+//        /// <summary>
+//        /// Returns (theoretically) unique base name that is suitable as the base filePath for saving this image
+//        /// </summary>
+//        /// <returns></returns>
+//        public string SaveNameBase()
+//        {
+//            if (string.IsNullOrEmpty(this.FileName))
+//                throw new Exception("FileName property not set");
+
+//            string saveName = Path.GetFileNameWithoutExtension(this.FileName);
+
+//            if (this.ImageNum >= 0)
+//                saveName += "_img" + this.ImageNum.ToString();
+
+//            return saveName;
+//        }
+
+//        /// <summary>
+//        /// Gets local filePath, if this exists - string.Empty otherwise
+//        /// </summary>
+//        public string FileName { get; protected set; }
+
+//        /// <summary>
+//        /// Gets 0-based image number in a multi-image file, -1 if image number is not relevant
+//        /// </summary>
+//        public int ImageNum { get; protected set; }
+
+//        /// <summary>
+//        /// Gets count of images in file
+//        /// </summary>
+//        /// <returns>image count, -1 if error</returns>
+//        public static int GetImageCount(string imageSpec)
+//        {
+//            if (string.IsNullOrEmpty(imageSpec))
+//                throw new ArgumentException("imageSpec not set", "imageSpec");
+
+//            int imgCount = -1;
+//            try
+//            {
+//                imgCount = ImageX.NumPages(AccuComponent.ImagXpress, imageSpec);
+//            }
+//            catch (Exception)
+//            {
+//            }
+
+//            return imgCount;
+//        }
+
+//        /// <summary>
+//        /// Load image and return .NET Bitmap object
+//        /// </summary>
+//        /// <returns>System.Drawing.Bitmap object with loaded image</returns>
+//        public Bitmap Load()
+//        {
+//            if (this._cacheBitmap != null)
+//                return this._cacheBitmap;   // quick return if already loaded
+
+//            if (string.IsNullOrEmpty(this.FileName))
+//                throw new Exception("FileName property not set");
+
+//            ImageX imgX = null;
+//            if (this.ImageNum >= 0)
+//                imgX = ImageX.FromFile(AccuComponent.ImagXpress, this.FileName, this.ImageNum + 1);
+//            else
+//                imgX = ImageX.FromFile(AccuComponent.ImagXpress, this.FileName);
+
+//            this._cacheBitmap = imgX.ToBitmap(true);
+//            imgX.Dispose();
+
+//            return this._cacheBitmap;
+//        }
+//    }
+//}
